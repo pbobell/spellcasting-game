5e43f45feb759d74e550b2e1a27149da1ed22ab8
Eric Willisson <epw@google.com>
2025-11-25 23:00:50 -0500

    Faster hand motion

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export var rotate_arcspeed: float = 1
+@export var rotate_arcspeed: float = 5
 var right_target: Vector3
 var left_target: Vector3
 
@@ -10,6 +10,19 @@ func _ready() -> void:
 
 func _input(_event: InputEvent) -> void:
 	pass
+
+func approach(current: float, target: float, speed: float) -> float:
+	if current == target:
+		return current
+	if absf(target - current) < speed:
+		return target
+	return current + signf(target - current) * speed
+
+func approachv3(current: Vector3, target: Vector3, speed: float) -> Vector3:
+	var result = Vector3()
+	for i in [0, 1, 2]:
+		result[i] = approach(current[i], target[i], speed)
+	return result
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -23,5 +36,7 @@ func _process(delta: float) -> void:
 	left_target.x = -left_hand.y
 	left_target.z = left_hand.x
 	
-	$Right.rotation = $Right.rotation.lerp(right_target, delta * rotate_arcspeed)
-	$Left.rotation = $Left.rotation.lerp(left_target, delta * rotate_arcspeed)
+#	$Right.rotation = $Right.rotation.lerp(right_target, delta * rotate_arcspeed)
+#	$Left.rotation = $Left.rotation.lerp(left_target, delta * rotate_arcspeed)
+	$Right.rotation = approachv3($Right.rotation, right_target, delta * rotate_arcspeed)
+	$Left.rotation = approachv3($Left.rotation, left_target, delta * rotate_arcspeed)
