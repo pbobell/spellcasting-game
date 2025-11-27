cabc1b8c65b9f91dea2bef18b66ecfc15a6beb56
Eric Willisson <epw@google.com>
2025-11-27 17:47:44 -0500

    Made approach() handle circles correctly.
    Well, probably correctly. It's about time we start using unit tests.

--- util.gd
+++ util.gd
@@ -5,6 +5,25 @@ extends Node
 ## The contents either are used in multiple scripts, or just have no business
 ## being stored in a particular node's script. Math utilities and the like.
 
+## Returns angle in range [-180, 180] or [-PI, PI].
+func normalize_angle(angle: float, radians: bool = false) -> float:
+	var circle = 360
+	if radians:
+		circle = TAU
+	while angle > circle / 2:
+		angle -= circle
+	while angle < -circle / 2:
+		angle += circle
+	return angle
+
+## Shortest distance between two angles, accounting for -180->180 singularity.
+func angle_to_angle(from: float, to: float, radians: bool = false) -> float:
+	var circle = 360
+	if radians:
+		circle = TAU
+	from = normalize_angle(from, radians)
+	to = normalize_angle(to, radians)
+	return fposmod(to-from + circle/2, circle) - circle
 
 ## Returns result of advancing current towards target by speed, or target if
 ## difference is smaller than speed.
@@ -12,18 +31,20 @@ extends Node
 ## [codeblock]
 ## x = approach(x, y, rate)
 ## [/codeblock]
-func approach(current: float, target: float, speed: float) -> float:
+func approach(current: float, target: float, speed: float, is_degrees: bool = false) -> float:
 	if current == target:
 		return target
 	if absf(target - current) < speed:
 		return target
+	if is_degrees:
+		return current + signf(angle_to_angle(current, target)) * speed
 	return current + signf(target - current) * speed
 
 ## Generalizes [method approach] to work on Vector3s.
-func approachv3(current: Vector3, target: Vector3, speed: float) -> Vector3:
+func approachv3(current: Vector3, target: Vector3, speed: float, is_degrees: bool = false) -> Vector3:
 	var result = Vector3()
 	for i in [0, 1, 2]:
-		result[i] = approach(current[i], target[i], speed)
+		result[i] = approach(current[i], target[i], speed, is_degrees)
 	return result
 
 ## Helper function to call [function deg_to_rad] on all elements of a Vector3.
