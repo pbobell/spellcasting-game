6c68a4ddee781697fa477b7be308c2c78241db5f
Eric Willisson <epw@google.com>
2025-11-29 21:39:13 -0500

    Multi-step animation system for enemy, and dpad controls to trigger debugging effects

--- enemy.gd
+++ enemy.gd
@@ -4,20 +4,41 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-func _ready() -> void:
-#	var universal = AnimationLibrary.new()
-#	universal.add_animation("idle", preload("res://assets/animations/idle.res"))
-#	universal.add_animation("punch_jab", preload("res://assets/animations/punch_jab.res"))
-#	$Orc/AnimationPlayer.add_animation_library("universal", universal)
-	$orc/AnimationPlayer.play("AnimationLibrary_Godot_Standard/Idle")
-	pass
+var ANIMATIONS = {
+	"punch": ["AnimationLibrary_Godot_Standard/Punch_Enter",
+			  "AnimationLibrary_Godot_Standard/Punch_Jab"],
+	"hit": ["AnimationLibrary_Godot_Standard/Hit_Chest"],
+	"headshot": ["AnimationLibrary_Godot_Standard/Hit_Head"],
+	"jump": ["AnimationLibrary_Godot_Standard/Jump_Start",
+			 "AnimationLibrary_Godot_Standard/Jump"]
+}
 
-func play_animation():
-	$orc/AnimationPlayer.play("AnimationLibrary_Godot_Standard/Punch_Jab")
+## Default animation to return to when queue is empty.
+@export var default_animation = "AnimationLibrary_Godot_Standard/Idle"
+
+## Animations to play before returning to default.
+var animation_queue = []
+
+func _ready() -> void:
+	play_default_animation()
+
+func play_default_animation() -> void:
+	$orc/AnimationPlayer.play(default_animation)
+
+func play_queue(new_queue = null) -> void:
+	if new_queue != null:
+		animation_queue = new_queue.duplicate()
+	if len(animation_queue) == 0:
+		play_default_animation()
+		return
+	var animation = animation_queue.pop_front()
+	$orc/AnimationPlayer.play(animation)
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
-		play_animation()
+	if event.is_action_pressed("dpad_up"):
+		play_queue(ANIMATIONS["jump"])
+	if event.is_action_pressed("dpad_right"):
+		play_queue(ANIMATIONS["punch"])
 
 func jump():
 	if is_on_floor():
@@ -32,8 +53,7 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
-	$orc/AnimationPlayer.play("AnimationLibrary_Godot_Standard/Idle")
+	play_queue()
 
-func hit_with_spell(ability: Node3D) -> void:
+func hit_with_spell(_ability: Node3D) -> void:
 	$orc/AnimationPlayer.play("AnimationLibrary_Godot_Standard/Hit_Chest")
-	
