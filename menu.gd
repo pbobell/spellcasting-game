f77b507700dea070570f30d4056e49f3f107982b
Eric Willisson <epw@google.com>
2025-12-07 11:26:37 -0500

    Code for menu

---
+++ menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@export var game: PackedScene = preload("res://main.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_packed(game)
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
